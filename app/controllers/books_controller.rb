dc9249e69318831dd6ec302544662fbf0a646581
EC2 Default User <ec2-user@ip-172-31-31-2.ap-northeast-1.compute.internal>
2023-11-13 11:44:04 +0000

    [Create] books#create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
+
+  # 投稿データの保存
+  def create
+    @book = Book.new(post_image_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to post_images_path
+  end
+
   def index
     @book = Book.new
   end
@@ -8,4 +17,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:book).permit(:body, :title)
+  end
+
 end
