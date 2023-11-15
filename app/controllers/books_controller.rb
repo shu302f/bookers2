5754208dc37702e0d0cea8dd99287e257866e936
EC2 Default User <ec2-user@ip-172-31-31-2.ap-northeast-1.compute.internal>
2023-11-15 09:38:05 +0000

    [Add] validations for book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      redirect_to :index
+      render :index
     end
   end
 
@@ -28,10 +28,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
