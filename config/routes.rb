96b78fd48a5b3dfffd8e44f2a7b2766a9a1a9f0d
EC2 Default User <ec2-user@ip-172-31-31-2.ap-northeast-1.compute.internal>
2023-11-18 06:11:31 +0000

    [Add] favorite model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   get "/home/about" => "homes#about", as: "about"
 end
